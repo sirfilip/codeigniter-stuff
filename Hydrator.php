63c90067f61baa4b88a47834a33016cd2b509ab2
Filip Kostovski <github.sirfilip@gmail.com>
2012-01-26 02:17:20 +0100

    Added has many hydration.

--- Hydrator.php
+++ Hydrator.php
@@ -15,27 +15,26 @@ class Hydrator {
 			if (array_key_exists($related, $sample->belongs_to()))
 			{
 				$belongs_to = $sample->belongs_to(); 
-				//$objects = $this->hydrate_belongs_to($belongs_to[$related], $objects);
-				$objects = $this->hydrate_belongs_to($belongs_to[$related], $objects);
+				$objects = $this->hydrate_belongs_to($related, $belongs_to[$related], $objects);
 			}
 
 			if (array_key_exists($related, $sample->has_many()))
 			{
 				$has_many = $sample->has_many();
-				$objects = $this->hydrate_has_many($has_many[$related], $objects);
+				$objects = $this->hydrate_has_many($related, $has_many[$related], $objects);
 			}
 
 			if (array_key_exists($related, $sample->has_and_belongs_to_many()))
 			{
 				$has_and_belongs_to_many = $sample->has_and_belongs_to_many();
-				$objects = $this->hydrate_has_and_belongs_to_many($has_and_belongs_to_many[$related], $objects);
+				$objects = $this->hydrate_has_and_belongs_to_many($related,$has_and_belongs_to_many[$related], $objects);
 			}
 		}
 
 		return $objects;
 	}
 
-	function hydrate_belongs_to($relation, &$objects) 
+	function hydrate_belongs_to($alias, $relation, $objects) 
 	{
 		require_once APPPATH."models/{$relation['model']}.php";
 		$ci = get_instance();
@@ -67,14 +66,51 @@ class Hydrator {
 
 		foreach ($objects as $object)
 		{
-			$alias = key($relation);
 			$object->{$alias} = $data[$object->{$relation['with_key']}];
 		}
 
 		return $objects;
 	}
 
-	function hydrate_has_many($relation, $objects) {}
+	function hydrate_has_many($alias, $relation, $objects) 
+	{
+		require_once APPPATH."models/{$relation['model']}.php";
+		$ci = get_instance();
+		$with_ids = array();
+		$with_key = isset($relation['with_key']) ? $relation['with_key'] : 'id';
+		foreach ($objects as $object)
+		{
+			$with_ids[] = $object->{$with_key};
+		}
+		$related_key = $relation['related_key'];
+		$query = $ci->db
+					->where_in($related_key, $with_ids)
+					->get("{$relation['related_table']}");
+		if ($query->num_rows() > 0)
+		{
+			$class_name = ucfirst($relation['model']);
+			$results = $query->result($class_name);
+		}
+		else
+		{
+			$results = array();
+		}
+		
+		$data = array();
+		
+		foreach ($results as $row)
+		{
+			$data[$row->{$related_key}] = isset($data[$row->{$related_key}]) ? $data[$row->{$related_key}] : array();
+			$data[$row->{$related_key}][] = $row;
+		}
+		
+		foreach ($objects as $object)
+		{
+			$object->{$alias} = isset($data[$object->{$with_key}]) ? $data[$object->{$with_key}] : array(); 
+		}
+		
+		return $objects;
+	}
 
 	function has_and_belongs_to_many($relation, $objects) {}
 
