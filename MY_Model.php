289590112ed57bb9a19c508bc5a3eef040189527
Filip Kostovski <github.sirfilip@gmail.com>
2012-07-05 15:50:25 +0200

    Wrote some simple doc comments in MY_Model

--- MY_Model.php
+++ MY_Model.php
@@ -1,6 +1,9 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-
+/**
+* Class that acts as DAO (data access object).
+* @author Filip Kostovski <filip.kostovski@cosmicdevelopment.com>
+*/
 class MY_Model extends CI_Model {
 	
 	protected $_table = NULL;
@@ -16,6 +19,9 @@ class MY_Model extends CI_Model {
 		// $this->db->save_queries = FALSE;
 	}
 
+	/**
+	* Delegates all method calls to db if exists.
+	*/ 
 	function __call($method, $params = array())
 	{
 		if (method_exists($this->db, $method))
@@ -25,6 +31,9 @@ class MY_Model extends CI_Model {
 		}
 	}
 	
+	/**
+	* Acts as getter and setter of the database table.
+	*/
 	function table($table = NULL)
 	{
 		if (is_null($table))
@@ -38,6 +47,12 @@ class MY_Model extends CI_Model {
 		}
 	}
 	
+	/**
+	* Acts as getter and setter of the DTO (data transfer object).
+	*
+	* DTO is the object used in generating query results by default it is std
+	* class object.
+	*/
 	function dto($class = NULL)
 	{
 		if (is_null($class))
@@ -51,11 +66,17 @@ class MY_Model extends CI_Model {
 		}
 	}
 	
+	/**
+	* Returns the primary key of a table
+	*/
 	function pk()
 	{
 		return $this->_primary_key;
 	}
 	
+	/**
+	* Fetches a single record as an instance of the dto setting.
+	*/
 	function get()
 	{
 		$query = $this->db->get($this->_table);
@@ -69,6 +90,9 @@ class MY_Model extends CI_Model {
 		}
 	}
 	
+	/**
+	* Fetches all records loaded from the db.
+	*/
 	function all()
 	{
 		$query = $this->db->get($this->_table);
@@ -83,6 +107,9 @@ class MY_Model extends CI_Model {
 		}
 	}
 	
+	/**
+	* Loads all records an returns an associative array with primary keys as key.
+	*/
 	function as_list($property)
 	{
 		$data = array();
@@ -96,17 +123,26 @@ class MY_Model extends CI_Model {
 		return $data;
 	}
 	
+	/**
+	* Shorthand for finding an object by id.
+	*/
 	function find_by_id($id)
 	{
 		return $this->where(array('id' => $id))->get();
 	}
 	
+	/**
+	* Alias for where.
+	*/
 	function find($where)
 	{
 		$this->db->where($where);
 		return $this;
 	}
 	
+	/**
+	* Fetches an object or raises 404.
+	*/
 	function get_object_or_404($where)
 	{
 		$object = $this->where($where)->get();
@@ -121,34 +157,52 @@ class MY_Model extends CI_Model {
 		}
 	}
 	
+	/**
+	* Creates new record in the databse.
+	*/
 	function create($props)
 	{
 		$this->db->insert($this->_table, $props);
 		return $this->db->insert_id();
 	}
 	
+	/**
+	* Updates existing record in the databse.
+	*/
 	function update($id, $props)
 	{
 		$this->db->where($this->pk(), $id)->update($this->_table, $props);
 		return $this->db->affected_rows();
 	}
 	
+	/**
+	* Deletes a record based on it's primary key.
+	*/
 	function delete($id)
 	{
 		$this->db->where($this->pk(), $id)->delete($this->_table);
 	}
 	
+	/**
+	* Performs pagination.
+	*/
 	function paginate($offset, $limit)
 	{
 		$this->db->offset($offset)->limit($limit);
 		return $this;
 	}
 	
+	/**
+	* Count all records loaded by the db.
+	*/
 	function count()
 	{
 		return $this->db->count_all_results($this->_table);
 	}
 	
+	/**
+	* Counts all records inside a table.
+	*/
 	function count_all()
 	{
 		return $this->db->count_all($this->_table);
